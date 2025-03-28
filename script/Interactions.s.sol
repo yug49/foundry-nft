479156b702d6b3f92d55add58646043d9b7792e8
yug49 <148035793+yug49@users.noreply.github.com>
2025-03-29 03:06:09 +0530

    Update Interactions.s.sol

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -3,7 +3,7 @@
 pragma solidity ^0.8.18;
 
 import {Script} from "../lib/forge-std/src/Script.sol";
-import {DevOpsTools} from "../lib/foundry-devOps/src/DevOpsTools.sol";
+import {DevOpsTools} from "../lib/foundry-devops/src/DevOpsTools.sol";
 import {BasicNft} from "../src/BasicNft.sol";
 
 contract MintBasicNft is Script {
